b07522dedc2f1780cb97781ca41cc3b3bcf46ac9
Eveline, Patricia Carolina <peveline3@gatech.edu>
2023-12-03 14:27:30 -0800

    Enabled measurement from 4 TCs

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,18 @@
 #include <max6675.h>
 
 // Hardware SPI pins
-int thermoCS1 = A0;  // Chip Select pin for the first MAX6675
 int thermoDO = MISO;
 int thermoCLK = SCK;
-// int thermoCS2 = A1;  // Chip Select pin for the second MAX6675 (if needed)
+int thermoCS1 = A0;  // Chip Select pin for the first MAX6675
+int thermoCS2 = A1;
+int thermoCS3 = A2;
+int thermoCS4 = A3;
 
 // Initialize the MAX6675 thermocouple amplifiers on the hardware SPI bus
 MAX6675 thermocouple1(thermoCLK, thermoCS1, thermoDO);
-// Adafruit_MAX6675 thermocouple2(SPI, thermoCS2);
-// Add more thermocouple objects if you have more thermocouples
+MAX6675 thermocouple2(thermoCLK, thermoCS2, thermoDO);
+MAX6675 thermocouple3(thermoCLK, thermoCS3, thermoDO);
+MAX6675 thermocouple4(thermoCLK, thermoCS4, thermoDO);
 
 #define SSR_PIN 5  // Define the SSR control pin. Ensure this doesn't conflict with other pins
 
@@ -32,16 +35,20 @@ void loop() {
   delay(1000);                // Wait for 1 second
 
   // Read temperature from the MAX6675s
-  Serial.print("Thermocouple 1 - C = "); 
-  Serial.println(thermocouple1.readCelsius());
   Serial.print("Thermocouple 1 - F = ");
   Serial.println(thermocouple1.readFahrenheit());
 
-  // Serial.print("Thermocouple 2 - C = "); 
-  // Serial.println(thermocouple2.readCelsius());
-  // Serial.print("Thermocouple 2 - F = ");
-  // Serial.println(thermocouple2.readFahrenheit());
+  Serial.print("Thermocouple 2 - F = ");
+  Serial.println(thermocouple2.readFahrenheit());
 
+  Serial.print("Thermocouple 3 - F = ");
+  Serial.println(thermocouple3.readFahrenheit());
+
+  Serial.print("Thermocouple 4 - F = ");
+  Serial.println(thermocouple4.readFahrenheit());
+
+  Serial.println("");
+  Serial.println("");
   // ... Repeat for additional thermocouples
 
   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads
